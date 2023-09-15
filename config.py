0bdb5258381cc13cace0ec32c76a568144235740
active <66892517+activ33@users.noreply.github.com>
2023-09-15 20:40:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = 1464463
 API_HASH = 'ff8451462d91861a13ffd8a6bb72aa8b'
-BOT_TOKEN = '6225958546:AAEdzjSVpeR4-lkJnVixhr_hlicvD-ISIn4'
+BOT_TOKEN = "6404643240:AAFCTb03FBn34kMVqaagNyj9tkyjGW6G5P0"
 ADMIN = 689061386
 CAPTION = 'Uploaded By 👉 @AnimeFiles'
 
